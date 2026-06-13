444b10d34c46b400bc9089576b4bc5608e325dbb
Abhishek Hosamani <abhishekhosamani522@gmail.com>
2026-06-13 20:25:41 +0530

    Fix: remove required attribute from file input to allow drag-and-drop submit

--- app/CreationForm.jsx
+++ app/CreationForm.jsx
@@ -317,7 +317,6 @@ export default function CreationForm({ onViewDashboard }) {
                                             onDragLeave={() => setIsDragging(false)}
                                             onDrop={handleDrop}
                                             className={styles.uploadInput} 
-                                            required 
                                         />
 
                                         {imagePreview ? (
